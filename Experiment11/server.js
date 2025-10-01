9311d0b4c33ba0f31c2e63e99c36402101df251d
Atharv Sharma <sharmaatharv7463@gmail.com>
2025-10-01 19:35:33 +0530

    commit

--- Experiment11/server.js
+++ Experiment11/server.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 let cards = [
   { id: 1, suit: "Hearts", value: "Ace" },
